3ac29d23e2e32c30c83aef3cb5226ce73fed2297
Ben Adrian Sarmiento <me@bensarmiento.com>
2014-02-16 12:17:44 +0800

    random pug

--- pugs.py
+++ pugs.py
@@ -1,8 +1,11 @@
 from pymongo import MongoClient
+import random
 
 client = MongoClient()
 db = client.makemehappy
 pugs = db.pugs
 
-pug = pugs.find_one()
+rand = random.random()*pugs.count()
+pug = pugs.find().limit(-1).skip(rand).next()
+
 print pug['url']
