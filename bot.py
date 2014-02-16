d9ca6d53c2925c87cf60bc85b2dccfbfcd67b87b
Ben Adrian Sarmiento <me@bensarmiento.com>
2014-02-17 00:44:00 +0800

    breaker, top

--- bot.py
+++ bot.py
@@ -6,51 +6,54 @@ import time
 import Image
 import magic
 
-client = MongoClient()
-db = client.makemehappy
-pugs = db.pugs
+for sr in ['pugs', 'pug', 'Puggifs']:
+    client = MongoClient()
+    db = client.makemehappy
+    pugs = db.pugs
 
-r = praw.Reddit(user_agent='pugsmakemehappy')
-submissions = r.get_subreddit('pugs+pug+Puggifs').get_new(limit=None)
-for x in submissions:
-    print ">>> "+x.url
-    not_a_gif = False
-    if pugs.find_one({"$or": [{"id": x.id}, {"url": x.url}]}):
-        print "Dupe: "+x.url
-        continue
-    else:
-        response = requests.get(x.url, stream=True)
-        while response.status_code != 200:
-            print "Request error: "+x.url
-            time.sleep(15)
+    r = praw.Reddit(user_agent='pugsmakemehappy')
+    submissions = r.get_subreddit(sr).get_top(limit=None)
+    xx = 0
+    for x in submissions:
+        xx += 1
+        print str(xx)+" >>> "+x.url
+        not_a_gif = 0
+        if pugs.find_one({"$or": [{"id": x.id}, {"url": x.url}]}):
+            print "Dupe: "+x.url
+            continue
+        else:
             response = requests.get(x.url, stream=True)
-        filepath = "/var/www/cess/makemehappy/test.gif"
-        with open(filepath, 'w') as f:
-            chunk = response.iter_content(256).next()
-            f.write(chunk)
-            mime = magic.from_buffer(chunk, mime=True)
-            if mime != "image/gif":
-                print mime+" not a gif: "+x.url
-                not_a_gif = True
-            for chunk in response.iter_content(256):
+            breaker = 5
+            while response.status_code != 200 and breaker >= 0:
+                breaker -= 1
+                print "Request error: "+x.url
+                time.sleep(15)
+                response = requests.get(x.url, stream=True)
+            if breaker < 0:
+                continue
+            filepath = "/var/www/cess/makemehappy/test.gif"
+            mime = "foo/bar"
+            with open(filepath, 'w') as f:
+                chunk = response.iter_content(256).next()
                 f.write(chunk)
-        # f.close()
-        if not_a_gif == False:
-            gif = Image.open(filepath)
-            try:
-                gif.seek(1)
-            except EOFError:
-                print "Not animated: "+x.url
+                mime = magic.from_buffer(chunk, mime=True)
+                if mime != "image/gif":
+                    print mime+" not a gif: "+x.url
+                    not_a_gif = 1
+                for chunk in response.iter_content(256):
+                    f.write(chunk)
+            # f.close()
+            if not_a_gif == 0:
+                gif = Image.open(filepath)
+                try:
+                    gif.seek(1)
+                except EOFError:
+                    print "Not animated: "+x.url
+                else:
+                    print "Animated: "+x.url
                 post = {"id": x.id,
                         "url": x.url,
                         "likes": 1,
-                        "animated": 0,
+                        "animated": not_a_gif,
+                        "mime": mime,
                         "date": datetime.datetime.utcnow()}
-            else:
-                print "Animated: "+x.url
-                post = {"id": x.id,
-                        "url": x.url,
-                        "likes": 1,
-                        "animated": 1,
-                        "date": datetime.datetime.utcnow()}
-                pugs.insert(post)
