12934641fa1b876f792fcdc5d3f7bcc8b7f8c15a
Ben Adrian Sarmiento <me@bensarmiento.com>
2014-02-16 12:28:16 +0800

    serve to web

--- pugs.py
+++ pugs.py
@@ -1,14 +1,17 @@
 from pymongo import MongoClient
 import random
+from flask import Flask
+app = Flask(__name__)
 
 client = MongoClient()
 db = client.makemehappy
 pugs = db.pugs
 
-x = 100
-while x > 0:
-	rand = int(random.random()*pugs.count())
-	print rand; x -= 1
-pug = pugs.find().limit(-1).skip(rand).next()
+@app.route("/")
+def hello():
+    rand = int(random.random()*pugs.count())
+    pug = pugs.find().limit(-1).skip(rand).next()
+    return '<img src="'+pug['url']+'">'
 
-print pug['url']
+if __name__ == "__main__":
+    app.run()
