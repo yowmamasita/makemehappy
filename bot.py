05c25c31d6f31ceb8644abc98afa09c433c983f6
Ben Adrian Sarmiento <me@bensarmiento.com>
2014-02-17 00:20:18 +0800

    gif tester 5

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ pugs = db.pugs
 r = praw.Reddit(user_agent='pugsmakemehappy')
 submissions = r.get_subreddit('Puggifs').get_new(limit=None)
 for x in submissions:
+    not_a_gif = False
     if pugs.find_one({"$or": [{"id": x.id}, {"url": x.url}]}):
         print "Dupe: "+x.url
         continue
@@ -24,28 +25,29 @@ for x in submissions:
             response = requests.get(x.url, stream=True)
         filepath = "/var/www/cess/makemehappy/test.gif"
         with open(filepath, 'w') as f:
-            mime = magic.from_buffer(response.iter_content().next(), mime=True)
+            mime = magic.from_buffer(response.iter_content(256).next(), mime=True)
             if mime != "image/gif":
-                print "Not a gif: "+x.url
-                continue
-            for chunk in response.iter_content():
+                print mime+" not a gif: "+x.url
+                not_a_gif = True
+            for chunk in response.iter_content(256):
                 f.write(chunk)
         # f.close()
-        gif = Image.open(filepath)
-        try:
-            gif.seek(1)
-        except EOFError:
-            print "Not animated: "+x.url
-            post = {"id": x.id,
-                    "url": x.url,
-                    "likes": 1,
-                    "animated": 0,
-                    "date": datetime.datetime.utcnow()}
-        else:
-            print "Animated: "+x.url
-            post = {"id": x.id,
-                    "url": x.url,
-                    "likes": 1,
-                    "animated": 1,
-                    "date": datetime.datetime.utcnow()}
-            pugs.insert(post)
+        if not not_a_gif:
+            gif = Image.open(filepath)
+            try:
+                gif.seek(1)
+            except EOFError:
+                print "Not animated: "+x.url
+                post = {"id": x.id,
+                        "url": x.url,
+                        "likes": 1,
+                        "animated": 0,
+                        "date": datetime.datetime.utcnow()}
+            else:
+                print "Animated: "+x.url
+                post = {"id": x.id,
+                        "url": x.url,
+                        "likes": 1,
+                        "animated": 1,
+                        "date": datetime.datetime.utcnow()}
+                pugs.insert(post)
