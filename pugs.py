5b3104140e50c2fb4092f307900c6890d799d8a3
Ben Adrian Sarmiento <me@bensarmiento.com>
2014-02-17 11:18:46 +0800

    flask

--- pugs.py
+++ pugs.py
@@ -1,14 +1,22 @@
 from pymongo import MongoClient
 import random
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 client = MongoClient()
 db = client.makemehappy
 pugs = db.pugs
 
-@app.route("/")
+@app.route('/')
 def hello():
+    return render_template('index.html')
+
+@app.route('/css')
+def css():
+    return url_for('static', filename='css/bootstrap.min.css')
+
+@app.route("/pugs")
+def pugs():
     rand = int(random.random()*pugs.find({"animated": 1}).count())
     pug = pugs.find({"animated": 1}).limit(-1).skip(rand).next()
     return pug['url']
