51fd386bd5e310ecf48b39a44ce6978aedf6b43f
Ben Adrian Sarmiento <me@bensarmiento.com>
2014-02-17 11:31:29 +0800

    flask 4

--- pugs.py
+++ pugs.py
@@ -16,10 +16,10 @@ def css():
     return url_for('static', filename='css/bootstrap.min.css')
 
 @app.route("/pugs")
-def pugs():
+def random_pugs():
     rand = int(random.random()*pugs.find({"animated": 1}).count())
     pug = pugs.find({"animated": 1}).limit(-1).skip(rand).next()
     return pug['url']
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
