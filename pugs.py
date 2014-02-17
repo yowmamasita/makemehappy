0aad2d7df4a140cce04e343e9e3a734b508e0000
Ben Adrian Sarmiento <me@bensarmiento.com>
2014-02-17 17:23:04 +0800

    FB login routes

--- pugs.py
+++ pugs.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 import random
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, session, request
 from flask_oauth import OAuth
 
 SECRET_KEY = 'makemehappy2014'
@@ -17,11 +17,38 @@ client = MongoClient()
 db = client.makemehappy
 pugs = db.pugs
 
+facebook = oauth.remote_app('facebook',
+    base_url='https://graph.facebook.com/',
+    request_token_url=None,
+    access_token_url='/oauth/access_token',
+    authorize_url='https://www.facebook.com/dialog/oauth',
+    consumer_key=FACEBOOK_APP_ID,
+    consumer_secret=FACEBOOK_APP_SECRET,
+    request_token_params={'scope': 'email'}
+)
+
 @app.route('/')
 def hello():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return facebook.authorize(callback=url_for('facebook_authorized',
+        next=request.args.get('next') or request.referrer or None,
+        _external=True))
 
+@app.route('/login/authorized')
+@facebook.authorized_handler
+def facebook_authorized(resp):
+    if resp is None:
+        return 'Access denied: reason=%s error=%s' % (
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+    session['oauth_token'] = (resp['access_token'], '')
+    me = facebook.get('/me')
+    return 'Logged in as id=%s name=%s redirect=%s' % \
+        (me.data['id'], me.data['name'], request.args.get('next'))
 
 # PUG GENERATORS
 
