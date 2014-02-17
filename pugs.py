a4d6621954d87002f5b2aa508b8aa1ebeaf82f6a
Ben Adrian Sarmiento <me@bensarmiento.com>
2014-02-17 17:15:19 +0800

    prepare for FB login

--- pugs.py
+++ pugs.py
@@ -1,7 +1,17 @@
 from pymongo import MongoClient
 import random
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
+from flask_oauth import OAuth
+
+SECRET_KEY = 'makemehappy2014'
+DEBUG = True
+FACEBOOK_APP_ID = '131978466977157'
+FACEBOOK_APP_SECRET = 'eedce7bad71c67e1c8e47e14822edb62'
+
 app = Flask(__name__)
+app.debug = DEBUG
+app.secret_key = SECRET_KEY
+oauth = OAuth()
 
 client = MongoClient()
 db = client.makemehappy
@@ -11,14 +21,26 @@ pugs = db.pugs
 def hello():
     return render_template('index.html')
 
-@app.route('/css')
-def css():
-    return url_for('static', filename='css/bootstrap.min.css')
+
+
+# PUG GENERATORS
 
 @app.route("/pugs")
 def random_pugs():
     rand = int(random.random()*pugs.find({"animated": 1}).count())
     pug = pugs.find({"animated": 1}).limit(-1).skip(rand).next()
+    return pug['url']
+
+@app.route("/pugs_static")
+def random_static_pugs():
+    rand = int(random.random()*pugs.find({"animated": 0}).count())
+    pug = pugs.find({"animated": 0}).limit(-1).skip(rand).next()
+    return pug['url']
+
+@app.route("/pugs_mixed")
+def random_mixed_pugs():
+    rand = int(random.random()*pugs.find().count())
+    pug = pugs.find().limit(-1).skip(rand).next()
     return pug['url']
 
 if __name__ == "__main__":
