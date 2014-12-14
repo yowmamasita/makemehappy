ef55edd39eca383e6dc8d208a8997811ad6b50f3
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2014-12-14 23:58:08 +0800

    refactor flask

--- pugs.py
+++ pugs.py
@@ -1,13 +1,15 @@
-from pymongo import MongoClient
-import random
 from flask import Flask, render_template, url_for, session, request, redirect, jsonify
 from flask_oauth import OAuth
+from pymongo import MongoClient
 import datetime
+import random
 
 SECRET_KEY = 'makemehappy2014'
 DEBUG = True
 FACEBOOK_APP_ID = '131978466977157'
 FACEBOOK_APP_SECRET = 'eedce7bad71c67e1c8e47e14822edb62'
+SITE_URL = 'http://pugs.makemehappy.bensarmiento.com'
+CALLBACK_URL = SITE_URL + '/login/authorized'
 
 app = Flask(__name__)
 app.debug = DEBUG
@@ -44,7 +46,7 @@ def hello():
 
 @app.route('/login')
 def login():
-    return facebook.authorize(callback='http://arch.cessallapitan.me/makemehappy/login/authorized')
+    return facebook.authorize(callback=CALLBACK_URL)
 
 
 @app.route('/logout')
@@ -56,33 +58,24 @@ def logout():
 @app.route('/login/authorized')
 @facebook.authorized_handler
 def facebook_authorized(resp):
+
     if resp is None:
         return 'Access denied: reason=%s error=%s' % (
             request.args['error_reason'],
             request.args['error_description']
         )
+
     session['oauth_token'] = (resp['access_token'], '')
     return redirect(url_for('hello', _external=True))
-
-
-@facebook.tokengetter
-def get_facebook_oauth_token():
-    return session.get('oauth_token')
-
-
-def is_number(s):
-    try:
-        float(s)
-        return True
-    except ValueError:
-        return False
 
 
 @app.route('/pug/')
 @app.route('/pug/<pug_id>')
 def that_pug(pug_id=None):
+
     if pug_id is None:
         return redirect(url_for('hello', _external=True))
+
     f_id = None
     name = None
     if session.get('oauth_token'):
@@ -100,8 +93,10 @@ def that_pug(pug_id=None):
 @app.route('/like/')
 @app.route('/like/<pug_id>')
 def like_pug(pug_id=None):
+
     if pug_id is None:
         return jsonify({'msg': 'error', 'error': 'pug_id missing'})
+
     if session.get('oauth_token'):
         me = facebook.get('/me')
         f_id = me.data['id']
@@ -121,7 +116,9 @@ def like_pug(pug_id=None):
             return jsonify({'msg': 'success'})
     else:
         return jsonify({'msg': 'error', 'error': 'you should login first'})
+
 # PUG GENERATORS
+
 
 @app.route("/pugs")
 def random_pugs():
@@ -129,11 +126,13 @@ def random_pugs():
     pug = pugs.find({"animated": 1}).limit(-1).skip(rand).next()
     return jsonify({'pug_id': str(pug['id']), 'likes': pug['likes'], 'url': pug['url']})
 
+
 @app.route("/pugs_static")
 def random_static_pugs():
     rand = int(random.random() * pugs.find({"animated": 0}).count())
     pug = pugs.find({"animated": 0}).limit(-1).skip(rand).next()
     return jsonify({'pug_id': str(pug['id']), 'likes': pug['likes'], 'url': pug['url']})
+
 
 @app.route("/pugs_mixed")
 def random_mixed_pugs():
@@ -143,3 +142,16 @@ def random_mixed_pugs():
 
 if __name__ == "__main__":
     app.run()
+
+
+@facebook.tokengetter
+def get_facebook_oauth_token():
+    return session.get('oauth_token')
+
+
+def is_number(s):
+    try:
+        float(s)
+        return True
+    except ValueError:
+        return False
