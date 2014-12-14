9248c85194bcafee6c70e5d784c46c0dd7870097
Ben Sarmiento <ben.sarmiento@cloudsherpas.com>
2014-12-14 10:14:47 +0800

    refactor tumblr bot

--- bots/tumblr.py
+++ bots/tumblr.py
@@ -6,13 +6,18 @@ import Image
 import magic
 
 API_KEY = "&api_key=d0wby4txhKrlfdHmN7WvAy5VDEs11ltp1ZOAH8xqQBgVZYxS5S"
-for tag in ['pug', 'pugs', 'pug+gif', 'pug+gifs']:
+SEARCH_TERMS = ['pug', 'pugs', 'pug+gif', 'pug+gifs']
+
+for tag in SEARCH_TERMS:
+    # build url
     url = "http://api.tumblr.com/v2/tagged?tag=" + tag + API_KEY
 
+    # setup db
     client = MongoClient()
     db = client.makemehappy
     pugs = db.pugs
 
+    # try searching for tag
     try:
         r = requests.get(url)
         breaker = 5
@@ -23,18 +28,27 @@ for tag in ['pug', 'pugs', 'pug+gif', 'pug+gifs']:
             r = requests.get(url)
     except:
         pass
+
     if breaker >= 0:
         data = r.json()
+
+        # iterate over each search result
         for resp in data['response']:
+
             not_a_gif = 0
             _id = resp['id']
+
             try:
                 photo = resp['photos'][0]['original_size']['url']
             except KeyError:
                 continue
+
             try:
+                # existing?
                 if pugs.find_one({"$or": [{"id": _id}, {"url": photo}]}):
                     continue
+
+                # download photo for analysis
                 response = requests.get(photo, stream=True)
                 breaker = 5
                 while response.status_code != 200 and breaker >= 0:
@@ -42,39 +56,50 @@ for tag in ['pug', 'pugs', 'pug+gif', 'pug+gifs']:
                     print "Request error: " + photo
                     time.sleep(15)
                     response = requests.get(photo, stream=True)
+
             except:
-                print "Request fatal error: " + photo
+                print "Request error: " + photo
                 continue
+
             if breaker < 0:
                 continue
-            filepath = "/var/www/cess/makemehappy/test2.gif"
-            mime = "foo/bar"
+
+            filepath = "tumblr.gif"
+
             with open(filepath, 'w') as f:
+
                 chunk = response.iter_content(256).next()
                 f.write(chunk)
                 mime = magic.from_buffer(chunk, mime=True)
+
                 if "image" not in mime:
-                    # print mime+" not a gif: "+photo
+                    # NOT A GIF
                     not_a_gif = 1
                 else:
+                    # continue download
                     for chunk in response.iter_content(256):
                         f.write(chunk)
-            # f.close()
+
+            # if it is a gif, check if animated
             if not_a_gif == 0:
                 animated = 0
                 gif = Image.open(filepath)
+
                 try:
                     gif.seek(1)
                 except EOFError:
+                    # NOT ANIMATED
                     animated = 0
-                    # print "Not animated: "+photo
                 else:
                     animated = 1
-                    print "Animated: " + photo
-                post = {"id": _id,
-                        "url": photo,
-                        "likes": 1,
-                        "animated": animated,
-                        "mime": mime,
-                        "date": datetime.datetime.utcnow()}
+
+                post = {
+                    "id": _id,
+                    "url": photo,
+                    "likes": 1,
+                    "animated": animated,
+                    "mime": mime,
+                    "date": datetime.datetime.utcnow()
+                }
                 pugs.insert(post)
+                print post
